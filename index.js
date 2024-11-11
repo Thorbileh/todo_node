56b369cd88b3380ae6026eb2a6a0e641f956720e
Thorbileh <preciousthobile99@gmail.com>
2024-11-11 14:50:37 +0200

    Added a pool file and .env file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Pool } = require('pg');
+const pool = require('./pool.js');
 const path = require('path');
 
 const app = express();
@@ -8,20 +8,7 @@ app.use(bodyParser.json());
 
 // Serve static files
 app.use(express.static(path.join(__dirname)));
-
-
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'tododb',
-    password: '',
-    port: 5432,
-})
-
 const PORT = 3000;
-
-
-
 
 app.listen(PORT, async (req, res) => {
     console.log(`Server is running on port ${PORT}`);
